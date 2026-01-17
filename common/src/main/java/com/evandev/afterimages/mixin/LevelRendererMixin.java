5245c96612912e901eb270a53a563c33b5ba4e78
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-17 00:37:29 -0800

    Translucency sorting

---
+++ common/src/main/java/com/evandev/afterimages/mixin/LevelRendererMixin.java
@@ -0,0 +1,35 @@
+package com.evandev.afterimages.mixin;
+
+import com.evandev.afterimages.client.AfterimageRenderer;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+
+    @Inject(
+            method = "renderLevel",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLorg/joml/Matrix4f;)V",
+                    shift = At.Shift.AFTER
+            ),
+            slice = @Slice(
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderType;translucent()Lnet/minecraft/client/renderer/RenderType;")
+            )
+    )
+    private void afterTranslucentLayer(PoseStack poseStack, float partialTick, long finishNanoTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightTexture lightTexture, Matrix4f projectionMatrix, CallbackInfo ci) {
+        var mc = Minecraft.getInstance();
+        AfterimageRenderer.renderAfterimages(poseStack, partialTick, mc.renderBuffers().bufferSource());
+    }
+}
